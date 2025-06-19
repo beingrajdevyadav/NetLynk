5e44224c9e20ddb6e437d9f912b3f594792eae19
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-19 22:48:18 +0530

    First ExpressJs API & Route Created and Tested.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,9 @@
 const express = require('express');
 
 const app = express();
+
+app.get('/', (req, res)=>{
+    res.send("API is running...");
+});
+
 app.listen(5000, console.log("Server is running on PORT 5000"));
