406e6ab7f159717bff007cadd377cd02a61b9051
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-21 20:35:00 +0530

    For Local APIs, Proxy Config In vite.config.js.

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 const Chat = () => {
+    const fetchData = async()=>{
+
+        const data = await axios.get("/api/chat");
+        console.log(data.data);
+    }
+
+   useEffect(()=>{
+    fetchData();
+   },[])
   return (
     <div>Chat</div>
   )
