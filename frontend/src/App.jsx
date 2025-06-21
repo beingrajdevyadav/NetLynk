113d1b74d1b1fb49f5764101364ec35f1248fc2c
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-21 20:05:02 +0530

    Chat Page Created.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Button, HStack } from "@chakra-ui/react"
 import './App.css'
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import Chat from "./pages/Chat"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       
 <Routes>
   <Route path="/" element={<Home/>} />
+  <Route path="/chat" element={<Chat/>} />
 </Routes>
     </>
   )
