adb77095c3e5ab6083438dcfcd1241ea7720de91
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-22 19:10:11 +0530

    UI Design Updated.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,16 +8,14 @@ const Home = () => {
         d={"flex"}
         justifyContent={"center"}
         p={3}
-        bg={"white"}
         w={"100%"}
         m={"40px 0 15px 0"}
         borderRadius={"lg"}
         borderWidth={"1px "}
-        color={"#444"}
         textAlign={"center"}
       >
 
-        <Text fontSize={"4xl"}>Make A Lynk</Text>
+        <Text fontSize={"4xl"}>NetLynk</Text>
 
       </Box>
 
@@ -25,8 +23,8 @@ const Home = () => {
       <Box  w={"100%"} borderRadius={"lg"} borderWidth={"1px"}>
         <Tabs.Root value={value} onValueChange={(e) => setValue(e.value)}>
           <Tabs.List color={"#444"} fontSize={"2xl"} justifyContent={"space-around"}>
-            <Tabs.Trigger value="first">First tab</Tabs.Trigger>
-            <Tabs.Trigger  value="second">Second tab</Tabs.Trigger>
+            <Tabs.Trigger value="first">Log In</Tabs.Trigger>
+            <Tabs.Trigger  value="second">Sign Up</Tabs.Trigger>
           </Tabs.List>
 
           <Tabs.Content    value="first">First panel</Tabs.Content>
