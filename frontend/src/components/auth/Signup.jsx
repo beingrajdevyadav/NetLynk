c5b2c37b8a1b40b7674a57a097a4d29eb0796192
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-22 21:55:01 +0530

    Sign Up Form Created.

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -8,8 +8,30 @@ const Signup = () => {
     const [password, setPassword] = useState();
     const [pic, setPic] = useState();
     return (
-       <div>
-        <h2>Sign Up</h2>
+       <div className='signup'>
+        <form action="">
+            <h2>Sign Up Form</h2>
+
+            <div className="form-control">
+                <input type="text" placeholder='Enter Your Name' value={name} onChange={(e)=>{setName(e.target.value)}} />
+            </div>
+            <div className="form-control">
+                <input type="email" placeholder='Enter Email' value={email} onChange={(e)=>{setEmail(e.target.value)}} />
+            </div>
+            <div className="form-control">
+                <input type="password" placeholder='Enter Password' value={password} onChange={(e)=>{setPassword(e.target.value)}} />
+            </div>
+            <div className="form-control">
+                <input type="password" placeholder='Confirm Password' value={confirmPassword} onChange={(e)=>{setConfirmPassword(e.target.value)}} />
+            </div>
+            <div className="form-control">
+                <input type="file" placeholder='' value={pic} onChange={(e)=>{setPic(e.target.value)}} />
+            </div>
+
+            <div className="btn-box">
+                <button type='submit'>Sign Up</button>
+            </div>
+        </form>
        </div>
     )
 }
