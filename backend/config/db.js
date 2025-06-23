a598ead4d93c95c6eb4236410882c18d6ee94c85
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-23 18:48:17 +0530

    MongoDB Connection Code Written.

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async ()=>{
+    try {
+       const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+        });
+        console.log(`MongoDB connected successfully : ${conn.connection.host}`.cyan.underline);
+    } catch (error) {
+        console.error('MongoDB connection failed:', error.message);
+        process.exit(1); // Exit the process with failure
+    }
+}
+
+module.exports = connectDB;
