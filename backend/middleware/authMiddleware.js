c612bcf55cd43a05b615c76094d37e16e6450d60
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-25 15:02:41 +0530

    authMiddleware Created.

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+const JWT = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
+const protect = asyncHandler(async (req, res, next) => {
+    let token;
+
+    // Check if the token is in the Authorization header
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            // Extract the token from the header
+            token = req.headers.authorization.split(' ')[1];
+
+            // Verify the token
+            const decoded = JWT.verify(token, process.env.JWT_SECRET);
+
+            // Find the user by ID from the decoded token
+            req.user = await User.findById(decoded.id).select('-password');
+
+            next(); // Proceed to the next middleware or route handler
+        } catch (error) {
+            res.status(401);
+            throw new Error('Not authorized, token failed');
+        }
+    } else {
+        res.status(401);
+        throw new Error('Not authorized, no token');
+    }
+});
+
+module.exports = { protect };
