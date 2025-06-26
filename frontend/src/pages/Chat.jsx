4e45f948894566fbf4a39ba696db8de9aaaf6590
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-26 21:28:43 +0530

    Chat UI Page Updated.

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,25 +1,10 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
+// import React, { useEffect, useState } from 'react'
 
 const Chat = () => {
-    const [data, setData]=useState([]);
-
-
-    const fetchData = async()=>{
-
-        const data = await axios.get("/api/chat");
-        setData(data.data);
-    }
-
-   useEffect(()=>{
-    fetchData();
-   },[])
+    
   return (
-    <div>{
-        data.map(c=>(
-            <h1 key={c.chatName}>{c.chatName}</h1>
-        ))
-        }</div>
+    <div><h1>Welcome To Chats Page</h1></div>
   )
 }
 
