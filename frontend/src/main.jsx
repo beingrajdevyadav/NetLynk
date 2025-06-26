54f578b54950673870dcd48dc7a37cc8c0fcc4fd
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-26 23:14:00 +0530

    Redux Store Used In Main.js To Wrap Whole App.jsx.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,17 +4,19 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from './components/ui/provider'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider as RedxProvider } from 'react-redux'
+import store from './redux/store'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  
+    <RedxProvider store={store}>
       <BrowserRouter>
         <Provider>
           <App />
         </Provider>
       </BrowserRouter>
-   
+    </RedxProvider>
   </StrictMode>
   ,
 )
