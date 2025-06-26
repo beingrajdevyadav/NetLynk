da351d6e53e86dd8f62e304ee06bcae512fa4a69
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-26 22:25:54 +0530

    Redux/Toolkit and React-Redux Installed.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,16 @@ import App from './App.jsx'
 import { Provider } from './components/ui/provider'
 import { BrowserRouter } from 'react-router-dom'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Provider>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </StrictMode>,
+  
+      <BrowserRouter>
+        <Provider>
+          <App />
+        </Provider>
+      </BrowserRouter>
+   
+  </StrictMode>
+  ,
 )
