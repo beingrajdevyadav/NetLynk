9d6be23166744679e2bab94ac2c2cd8ec38c6b39
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-27 16:53:21 +0530

    Sidebar Component Created.

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
+const Sidebar = ({ isOpen, onClose }) => {
+    const sidebarRef = useRef();
+
+    useEffect(() => {
+        const handleclickOutside = (e) => {
+            if (sidebarRef.current && !sidebarRef.current.contains(e.target)) {
+                onClose();
+            }
+        };
+
+        if (isOpen) {
+            document.addEventListener("mousedown", handleclickOutside);
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleclickOutside);
+        };
+    }, [isOpen, onClose]);
+
+
+    if (!isOpen) return null;
+
+    return (
+        <div className='overlay'>
+            <div className="sidebar" ref={sidebarRef}>
+                <h2>Sidebar Content</h2>
+                <button onClick={onClose}>close</button>
+            </div>
+        </div>
+    )
 }
 
 export default Sidebar
