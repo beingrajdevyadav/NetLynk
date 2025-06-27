ef07a51a5b05fa7ff2429036e7277571b3ec4170
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-27 16:43:07 +0530

    Chat Page UI Updated.

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,8 +1,8 @@
 // import axios from 'axios'
 import React, { useEffect } from 'react'
-
 import { useSelector } from "react-redux"
 import { useNavigate } from 'react-router-dom';
+import '../css/chats.css'
 
 const Chat = () => {
   const currentUser = useSelector(state => state.user.currentUser);
@@ -15,15 +15,56 @@ const Chat = () => {
     }
   }, [currentUser, navigate]);
   return (
-    <div className='chat-page'>
-      <div className="header">
-        <div className="search-bar"></div>
-        <div className="title-bar"></div>
-        <div className="menu-bar">
-          
+    <>
+
+      <div className='chat-page'>
+        <div className="header">
+          <div className="title-bar">
+            <h2>NetsApp</h2>
+          </div>
+
+          <div className="menu-bar">
+            <div className="search">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </div>
+
+            <div className="bell">
+              <i className="fa-solid fa-bell"></i>
+            </div>
+
+            <div className="user">
+              <img src={currentUser.pic} alt="" />
+            </div>
+          </div>
+        </div>
+
+        <div className="chat-container">
+          <div className="chat-lists">
+            <div className="list-header">
+              <h3>My Chats</h3>
+
+              <div className="create-icon">
+                <i className="fa-solid fa-plus"></i>
+                <span> Create Group </span>
+              </div>
+            </div>
+          </div>
+          <div className="open-chat"></div>
+        </div>
+
+
+
+        <div className="sidedrawer">
+          <div className="search-field">
+            <input type="text" placeholder='Search Here' />
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </div>
+          <div className="results-list"></div>
         </div>
       </div>
-    </div>
+
+
+    </>
   )
 }
 
