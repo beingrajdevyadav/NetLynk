4b9a96e1d05bef78537bc24af1b0d08cd8fdbb6d
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-27 10:13:32 +0530

    Dyamic Page Navigation Functionality Added Based On CurrentUser Logged In or Not.

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,8 +1,19 @@
 // import axios from 'axios'
-// import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+
+import { useSelector } from "react-redux"
+import { useNavigate } from 'react-router-dom';
 
 const Chat = () => {
-    
+  const currentUser = useSelector(state => state.user.currentUser);
+  const navigate = useNavigate();
+  console.log(currentUser);
+
+  useEffect(() => {
+    if (!currentUser) {
+      navigate('/');
+    }
+  }, [currentUser, navigate]);
   return (
     <div><h1>Welcome To Chats Page</h1></div>
   )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Box, Container, Tabs, Text } from '@chakra-ui/react'
 import Login from '@/components/auth/Login';
 import Signup from '@/components/auth/Signup';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 const Home = () => {
   const [value, setValue] = React.useState('first');
+  const currentUser = useSelector(state => state.user.currentUser);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (currentUser) {
+      navigate("/chats");
+    }
+  })
   return (
     <Container maxW={'xl'} centerContent>
       <Box
@@ -22,15 +32,15 @@ const Home = () => {
       </Box>
 
 
-      <Box  w={"100%"} borderRadius={"lg"} borderWidth={"1px"}>
+      <Box w={"100%"} borderRadius={"lg"} borderWidth={"1px"}>
         <Tabs.Root value={value} onValueChange={(e) => setValue(e.value)}>
           <Tabs.List color={"#444"} fontSize={"2xl"} justifyContent={"space-around"}>
             <Tabs.Trigger value="first">Log In</Tabs.Trigger>
-            <Tabs.Trigger  value="second">Sign Up</Tabs.Trigger>
+            <Tabs.Trigger value="second">Sign Up</Tabs.Trigger>
           </Tabs.List>
 
-          <Tabs.Content    value="first"><Login/></Tabs.Content>
-          <Tabs.Content value="second"><Signup/></Tabs.Content>
+          <Tabs.Content value="first"><Login /></Tabs.Content>
+          <Tabs.Content value="second"><Signup /></Tabs.Content>
         </Tabs.Root>
       </Box>
     </Container>
