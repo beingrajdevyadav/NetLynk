a8c3253b741ee7210fe8799fdb89a3c9b5d4f776
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-27 10:23:26 +0530

    Chakra UI Materials Removed.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,21 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Provider } from './components/ui/provider'
+
 import { BrowserRouter } from 'react-router-dom'
-import { Provider as RedxProvider } from 'react-redux'
+import { Provider } from 'react-redux'
 import store from './redux/store'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RedxProvider store={store}>
+    <Provider store={store}>
       <BrowserRouter>
-        <Provider>
+    
           <App />
-        </Provider>
+      
       </BrowserRouter>
-    </RedxProvider>
+    </Provider>
   </StrictMode>
   ,
 )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react'
-import { Box, Container, Tabs, Text } from '@chakra-ui/react'
+
 import Login from '@/components/auth/Login';
 import Signup from '@/components/auth/Signup';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 const Home = () => {
-  const [value, setValue] = React.useState('first');
   const currentUser = useSelector(state => state.user.currentUser);
   const navigate = useNavigate();
 
@@ -15,35 +14,11 @@ const Home = () => {
     }
   })
   return (
-    <Container maxW={'xl'} centerContent>
-      <Box
-        d={"flex"}
-        justifyContent={"center"}
-        p={3}
-        w={"100%"}
-        m={"40px 0 15px 0"}
-        borderRadius={"lg"}
-        borderWidth={"1px "}
-        textAlign={"center"}
-      >
-
-        <Text fontSize={"4xl"}>NetLynk</Text>
-
-      </Box>
-
-
-      <Box w={"100%"} borderRadius={"lg"} borderWidth={"1px"}>
-        <Tabs.Root value={value} onValueChange={(e) => setValue(e.value)}>
-          <Tabs.List color={"#444"} fontSize={"2xl"} justifyContent={"space-around"}>
-            <Tabs.Trigger value="first">Log In</Tabs.Trigger>
-            <Tabs.Trigger value="second">Sign Up</Tabs.Trigger>
-          </Tabs.List>
-
-          <Tabs.Content value="first"><Login /></Tabs.Content>
-          <Tabs.Content value="second"><Signup /></Tabs.Content>
-        </Tabs.Root>
-      </Box>
-    </Container>
+    
+<>
+<Login />
+<Signup />
+</>
   )
 }
 
