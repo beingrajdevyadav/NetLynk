f4e8c2ee9f4aab74c7fabedd15913f8aa3719b60
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-06-28 19:02:19 +0530

    Home Page UI Updated.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,10 +3,10 @@ import Login from '../components/auth/Login';
 import Signup from '../components/auth/Signup';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-
+import '../css/home.css'
 
 const Home = () => {
-  const [isLog, setIsLog] =  useState(true);
+  const [isLog, setIsLog] = useState(true);
 
   const currentUser = useSelector(state => state.user.currentUser);
   const navigate = useNavigate();
@@ -17,28 +17,28 @@ const Home = () => {
     }
   })
   return (
-    
-<>
-<div className="home">
- 
-  <div className="home-container">
 
-     <div className="home-header">
-    <h1>NetsApp</h1>
-  </div>
-  <div className="home-controls">
-    <button onClick={()=>setIsLog(true)}>Log In</button>
-    <button onClick={()=>setIsLog(false)}>Sign Up</button>
-  </div>
+    <>
+      <div className="home">
+
+        <div className="home-container">
+          <div className="home-header">
+            <h2>NetsApp</h2>
+          </div>
+
+          <div className="home-controls">
+            <button className={isLog?"active":""} onClick={() => setIsLog(true)}>Log In</button>
+            <button className={!isLog?"active":""}  onClick={() => setIsLog(false)}>Sign Up</button>
+          </div>
+
+<hr />
+          {isLog ? <Login /> : <Signup />}
+        </div>
 
 
-    {isLog?<Login /> :<Signup />  }
-  </div>
+      </div>
 
-
-</div>
-
-</>
+    </>
   )
 }
 
